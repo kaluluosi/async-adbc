baf009f63ded3a1dd3ead6d8e9c515fb880a9919
kaluluosi111 <kaluluosi@gmail.com>
2023-05-18 14:57:33 +0800

    am startapp接口改为只传包名

--- async_adbc/plugins/am.py
+++ async_adbc/plugins/am.py
@@ -2,13 +2,11 @@ from . import Plugin
 
 
 class ActivityManagerPlugin(Plugin):
-    async def start_app(self, package_name: str, active_name: str = "MainActivity"):
+    async def start_app(self, package_name: str):
         """这个方法应该能够支持直接打开应用的某个Activity
 
         Args:
             package_name (str): 包名
             active_name (str, optional): 目标Activity. Defaults to "MainActivity".
         """
-        await self._device.shell(
-            f"am start -n {package_name}/{package_name}.{active_name}"
-        )
+        await self._device.shell(f"am start {package_name}")
