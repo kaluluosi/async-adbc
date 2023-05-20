1ee0477c6337d88defcf64e58408e2d1f1ec06c4
kaluluosi <kaluluosi@gmail.com>
2023-05-20 11:36:38 +0800

    增加exceptions模块来汇总所有异常类型

---
+++ async_adbc/exceptions.py
@@ -0,0 +1,3 @@
+from async_adbc.adbclient import DeviceNotFoundError
+from async_adbc.plugins.fps import SurfaceNotFoundError
+from async_adbc.plugins.pm import InstallError, UninstallError, ClearError
